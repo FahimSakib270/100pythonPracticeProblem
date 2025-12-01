c625ed611bbdebdd713814587ab65b5d5958c89d
FahimX <sakibfahim270@gmail.com>
2025-12-02 05:29:01 +0800

    QUESTION_4 SOLVED

---
+++ Qyestion_4.py
@@ -0,0 +1,9 @@
+# Write a program that will give you the sum of 3 digits
+
+num1 = int(input("Enter number "))
+num2 = int(input("Enter number "))
+num3 = int(input("Enter number "))
+
+sum = num1+num2+num3
+
+print(sum)
